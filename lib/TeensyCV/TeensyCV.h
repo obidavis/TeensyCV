7eb7f09c1746b012fed659a50646d649306cebf3
Obi <obi@Obis-Mac-mini.local>
2022-07-23 07:45:33 +0100

    cleanup

--- lib/TeensyCV/TeensyCV.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "ObjectDetection/BackgroundSubtractor.h"
-#include "ObjectDetection/ThresholdDetector.h"
-#include "ObjectDetection/gmg.h"
-#include "ObjectDetection/GMGBackgroundSubtractor.h"
